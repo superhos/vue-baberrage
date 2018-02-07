8fbc571958e9c9bf94ec07000ad7f96f2261ffab
stevenchen <stevenchenctm@gmail.com>
2018-02-07 11:36:44 +0800

    1) verison 1.2.0 update

--- src/lib/vue-baberrage.vue
+++ src/lib/vue-baberrage.vue
@@ -58,7 +58,7 @@ export default {
   },
   mounted () {
     window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
-
+    
     window.cancelAnimationFrame = window.cancelAnimationFrame ||
       window.mozCancelAnimationFrame || function (requestID) { clearTimeout(requestID) }
 
