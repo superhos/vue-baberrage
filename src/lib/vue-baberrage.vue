c0d3962952676a65a56304f8ae036ed8d442a716
superhos <297495165@qq.com>
2018-06-21 16:50:20 +0800

    1. 使用CSS动画作为弹幕核心，减少JS运算损耗。(暂未支持竖向弹幕)

--- src/lib/vue-baberrage.vue
+++ src/lib/vue-baberrage.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="baberrage-stage" v-show="isShow" ref="stage">
     <div class="baberrage-top">
-      <div v-for="item in topQueue" v-bind:style="item.style" v-bind:key="item.id" class="baberrage-item" v-bind:class="item.barrageStyle">
+      <div v-for="item in topQueue" :id="'barrage_top_' + item.id" v-if="item.isMoving" :style="item.style" :key="item.id" class="baberrage-item" :class="item.barrageStyle">
         <div class="baberrage-avatar"><img :src="item.avatar"></div>
         <div class="baberrage-msg">{{ item.msg }}</div>
       </div>
     </div>
-    <div v-for="item in barrageList" v-bind:style="item.style" v-bind:key="item.id"  class="baberrage-item" v-bind:class="item.barrageStyle">
+    <div v-for="item in barrageList" :id="'barrage_right_' + item.id" :style="item.style" :key="item.id"  class="baberrage-item" :class="item.barrageStyle">
       <div class="baberrage-avatar"><img :src="item.avatar"></div>
       <div class="baberrage-msg">{{ item.msg }}</div>
     </div>
     <div class="baberrage-bottom">
-      <div v-for="item in bottomQueue" v-bind:key="item.id" v-bind:style="item.style" class="baberrage-item" v-bind:class="item.barrageStyle">
+      <div v-for="item in bottomQueue" :id="'barrage_bottom_' + item.id" :key="item.id" :style="item.style" class="baberrage-item" :class="item.barrageStyle">
         <div class="baberrage-avatar"><img :src="item.avatar"></div>
         <div class="baberrage-msg">{{ item.msg }}</div>
       </div>
@@ -51,9 +51,11 @@ export default {
       boxHeightVal: this.boxHeight,
       loopVal: this.loop,
       startTime: 0,
+      transitionend: null,
       frameId: null,
       topQueue: [], // 顶部队列
-      bottomQueue: [] // 底部队列
+      bottomQueue: [], // 底部队列
+      cssPool: [],//CSS池
     }
   },
   mounted () {
@@ -71,6 +73,7 @@ export default {
       this.boxHeightVal = this.boxHeightVal === 0 ? window.innerHeight : this.boxHeightVal
     }
 
+    this.taskLoop();
     this.play()
   },
   watch: {
@@ -80,21 +83,85 @@ export default {
   },
   methods: {
     // 更新弹幕数据
-    updateBarrageDate (timestamp) {
-      if (this.startTime == null) this.startTime = timestamp
-      if (typeof timestamp !== 'undefined') {
-        this.move(timestamp)
-      }
-      if (this.barrageList.length > 0 || this.topQueue.length > 0 || this.bottomQueue.length > 0) {
-        this.play()
-      } else {
+    updateBarrageDate () {
+      //开始移动
+      this.moving()
+      if (!(this.barrageList.length > 0 || this.topQueue.length > 0 || this.bottomQueue.length > 0)) {
         // 如果弹幕序列为空发出事件 barrageListEmpty
         this.$emit('barrage-list-empty')
       }
     },
-    // 开始弹幕
-    play () {
-      this.frameId = requestAnimationFrame(this.updateBarrageDate)
+    moving () {
+      for (var i in this.barrageList){
+        var item = this.barrageList[i];
+        if (item.isMoving === undefined){
+          this.itemReset(item);
+          item.barrageStyle = ' normal ' + this.packageCSS(item.top,item.width,item.time);//'move'+item.time;
+          this.$set(item, 'style', {
+            width: item.width + 'px'
+          })
+          item.isMoving = true;
+          var _this = this;
+          this.$nextTick(function () {
+            _this.moveEnd(item);
+          })
+        }
+      }
+    },
+    packageCSS (y,width,time) {
+        //使用随机css池中的css
+        var id = Math.floor(Math.random() * 30 + 1);
+        if (this.cssPool[id]){
+          return 'move_' + id;
+        }
+        this.addKeyFrames(
+            '.move_' + id,
+            'animation-duration: '+time+'s;' + 
+            'transition-timing-function: linear;' +
+            'animation-name: moving_' + id + ';' + 
+            'animation-fill-mode: forwards;',
+            true
+        );
+        this.addKeyFrames(
+            'moving_' + id,
+            '0%{transform:translate3d('+this.boxWidthVal+'px,'+y+'px,0)}' + 
+            '99%{transform:translate3d(-'+(width+200)+'px,'+y+'px,0);opacity:1;}' +
+            '100%{transform:translate3d(-'+(width+200)+'px,'+y+'px,0);opacity:0;}'
+        );
+        this.cssPool[id] = true;
+        return 'move_' + id;
+    },
+    addKeyFrames (name,frames,css) {
+      var myReuseableStylesheet = document.createElement('style');//('style'), addKeyFrames = null;
+      document.head.appendChild( myReuseableStylesheet );
+      if (!window.createPopup) { /* For Safari */
+        myReuseableStylesheet.appendChild(document.createTextNode(''));
+      }
+      var styleSheet = myReuseableStylesheet;
+      if (css){
+        styleSheet.innerHTML +=  name + "{" + frames + "}";
+      }else{
+        styleSheet.innerHTML += "@keyframes " + name + "{" + frames + "}";
+      }
+    },
+    moveEnd (item) {
+      
+    },
+    taskLoop () {
+      this.frameId = requestAnimationFrame(this.checkBarrageList);
+    },
+    checkBarrageList (timestamp) {
+      var item;
+      for (var i in this.barrageList){
+        item = this.barrageList[i];
+        if (item.startTime == undefined && item.isMoving){
+          item.startTime = timestamp;
+        }
+        if ((timestamp - item.startTime)/1000 > item.time){
+          this.barrageList.splice(i,1);
+        }
+      }
+      this.taskLoop();
     },
     // 暂停弹幕
     pause () {
@@ -107,79 +174,11 @@ export default {
       }
       this.play()
     },
-    // 弹幕移动
-    move (timestamp) {
-      for (var i in this.barrageList) {
-        var item = this.barrageList[i]
-        if (typeof item.startTime !== 'undefined') {
-          if (item.type === 0) {
-            // 正常弹幕
-            this.normalMove(item, timestamp)
-            // 退出条件
-            if (item.left + item.width < 0) {
-              if (this.loopVal) {
-                // 如果循环则不删除数据
-                this.itemReset(item, timestamp)
-              } else {
-                // 不循环则删除数据
-                this.barrageList.splice(i, 1)
-              }
-            }
-          } else {
-            if (item.position !== 'top' && item.position !== 'bottom') {
-              throw new Error('Position only between top and bottom when the type equal 1')
-            }
-            // 固定弹幕
-            this.fixMove(item, timestamp)
-            this.barrageList.splice(i, 1)
-          }
-        } else {
-          // 弹幕初始化
-          this.itemReset(item, timestamp)
-        }
-      }
-
-      // 更新队列
-      this.queueRefresh(timestamp)
-    },
-    // 正常移动
-    normalMove (item, timestamp) {
-      // 时间差
-      var progress = timestamp - item.currentTime
-      item.currentTime = timestamp
-      // 移动距离
-      var moveVal = item.speed * progress
-
-      // 如果移动距离为0或者NaN 跳过，保持动画连续和减少重绘
-      if (moveVal <= 0 || isNaN(moveVal)) return
-      item.left -= moveVal
-      // 设置移动
-      this.$set(item, 'style', {
-        transform: 'translate3d(' + item.left + 'px,' + item.top + 'px,0px)',
-        width: item.width + 'px'
-      })
-    },
     // 固定弹幕
     fixMove (item, timestamp) {
       // 判断是否在队列中
       if (this[item.position + 'Queue'].indexOf(item) === -1) {
         this[item.position + 'Queue'].push(item)
-      }
-    },
-    // 队列数据刷新
-    queueRefresh (currentTime) {
-      var item
-      for (var i in this.topQueue) {
-        item = this.topQueue[i]
-        if (item.startTime + item.time * 1000 <= currentTime) {
-          this.topQueue.shift()
-        }
-      }
-      for (var j in this.bottomQueue) {
-        item = this.bottomQueue[j]
-        if (item.startTime + item.time * 1000 <= currentTime) {
-          this.bottomQueue.shift()
-        }
       }
     },
     itemReset (item, timestamp) {
@@ -192,7 +191,7 @@ export default {
       // this.position = 'normal'; // normal:随机 top:顶部 bottom:底部
 
       item.type = (typeof item.type === 'undefined') ? 0 : parseInt(item.type)
-      item.position = (typeof item.position === 'undefined') ? 'top' : item.position
+      item.position = (typeof item.position === 'undefined') ? 'right' : item.position
       item.barrageStyle = (typeof item.barrageStyle === 'undefined') ? 'normal' : item.barrageStyle
       item.startTime = timestamp
       item.currentTime = timestamp
@@ -209,9 +208,6 @@ export default {
           item.top = this.boxHeightVal - (this[item.position + 'Queue'].length * 45 + 100)
         }
       }
-      this.$set(item, 'style', {
-        transform: 'translate3d(' + item.left + 'px,' + item.top + 'px,0)'
-      })
     },
     // ========================= Tools ===========================
     extend (defaultSetting, option) {
@@ -278,4 +274,21 @@ export default {
       color:#FFF;
     }
 }
+
+.move-old {
+  animation-duration: 8s;
+  animation-name: moving;
+}
+
+@keyframes moving-old {
+  from {
+    transform: translate3d(130%, 300px, 0px);
+  }
+
+  to {
+    transform: translate3d(-30%, 300px, 0px);
+    opacity: 0;
+  }
+}
+
 </style>
