670c4b31184d73b3f20b63e066fe20bfb7265976
superhos <297495165@qq.com>
2020-01-07 00:17:47 +0800

    1. 改用LESS

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/lib/vue-baberrage.vue
+++ src/lib/vue-baberrage.vue
@@ -234,7 +234,7 @@ export default {
 
 </script>
 
-<style lang="scss">
+<style lang="less">
 .baberrage-stage{
   position: absolute;
     width: 100%;
